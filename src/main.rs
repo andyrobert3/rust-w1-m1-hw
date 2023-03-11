b5d2e1c66be617c913a350e74adc758b1693770b
jakeyvee <jakeyveee@gmail.com>
2023-03-11 20:17:46 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+// Define a User struct which contains 2 fields: 
+    // name (string e.g "John")
+    // balance (tuple e.g (100.00, "SGD"))
+
+// Define a User method (using impl) 
+    // called print_user_detail, which simply prints the name, balance and currency of the user.
+
+// Define an accrue_interest function, which takes in a user and interest percentage as 2 separate parameters. 
+    // Within the function, increase the users' balance by the interest percentage, and print out the user details by calling the print_user_detail method. 
+
+fn main() {
+    // In the main function, create a user variable of type User, populating the field values of name, and balance and currency. Then, call the accrue_interest function.
+    // Bonus: After the call to accrue_interest, call it multiple times so that the user may benefit from compounding interest.
+}
